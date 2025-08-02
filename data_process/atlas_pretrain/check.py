8433d327126853fe754f7e1d0808e72cafe0462d
Xinxu Wei <40337178+weixinxu666@users.noreply.github.com>
2025-08-01 23:01:22 -0400

    Delete data_process directory

--- data_process/atlas_pretrain/check.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-data = np.load('/home/xinxu/Lehigh/Codes/BICLab_data/atlas_group/combined_Schaefer200.npy', allow_pickle=True)
-
-print('ok')
--- data_process/read_labels/mat2struct.m
+++
@@ -1,4 +0,0 @@
-data = load('/home/xinxu/Lehigh/Codes/BICLab_data/ADNI/rsfMRI_ROISignals_phenotypic_ADNI2_ADNI3_20231210.mat')
-
-data = table2struct(data.fMRIdata_ADNI2_ADNI3)
-save('/home/xinxu/Lehigh/Codes/BICLab_data/ADNI/adni23_label.mat', 'data')
